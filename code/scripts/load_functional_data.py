169be7030d50d80fd21ae724cdc402eeb1c1e808
sashatinelli439 <sasha.tinelli@chch.ox.ac.uk>
2025-09-02 19:00:32 +0000

    added stim information to the loops

--- code/scripts/load_functional_data.py
+++ code/scripts/load_functional_data.py
@@ -16,6 +16,7 @@ import collections
 from hdmf_zarr import NWBZarrIO
 from nwbwidgets import nwb2widget
 import tqdm as tqdm
+import pickle
 
 # Set the utils path
 utils_dir = pjoin("..", "utils")
@@ -29,37 +30,6 @@ from data_io import _get_data_dir
 # get metadata
 data_dir = '/data/'
 metadata = pd.read_csv(os.path.join(data_dir, 'metadata', 'V1DD_metadata.csv'))
-
-#%% get subject ids
-subject_ids = np.sort(metadata['subject_id'].unique())
-subject_id = subject_ids[0]
-
-#%% get one mouse metadata
-this_mouse_metadata = metadata[metadata['subject_id']==subject_id].sort_values(by='session_date')
-
-#%% get this mouse directory
-data_dir = '/data/'
-mouse_id = str(subject_id)
-mouse_dir = glob.glob((os.path.join(data_dir, mouse_id+'*')))[0]
-mouse_dir
-
-#%% get session
-session_name = this_mouse_metadata.name.values[-1]
-session_name
-
-#%% get session dir
-session_dir = os.path.join(mouse_dir, session_name)
-print(session_dir)
-
-#%% get nwb file path
-nwb_file = [file for file in os.listdir(session_dir) if 'nwb' in file][0]
-nwb_path = os.path.join(session_dir, nwb_file)
-print(nwb_path)
-
-#%% open nwb file
-with NWBZarrIO(str(nwb_path), 'r') as io:
-    nwbfile = io.read()
-    print('Loaded NWB file from:', nwb_path)
 
 #%% get the session list for one animal
 def get_sessions(mouse_id, data_dir):
@@ -102,16 +72,16 @@ def interp_across_planes(nwbfile, column, volume, remove_known_bad_planes=True):
         "plane": planes_traces,
         "roi": roi_traces,
         "base_time": base_times}
-#%%
-col_vol = []
-        for col in this_mouse_metadata["column"].unique():
-            for vol in this_mouse_metadata.loc[this_mouse_metadata["column"] == col, "volume"].unique():
-                if pd.isna(vol): 
-                    continue
-                else:
-                    col_vol.append([col, vol])
-#%% loop over sessions
 
+#%% pull stim information
+def pull_stim_info(nwbfile):
+    stim_df = []
+    stim = nwbfile.epochs.to_dataframe().reset_index(drop=True)
+    keep = ["stim_name", "start_time", "stop_time", "duration"]
+    return stim[keep].copy()
+
+
+#%% loop over sessions
 def pre_process(mouse_ids=None, sessions=None, data_dir = '/data/'):
     if mouse_ids is None:
         mouse_ids = ['409828']
@@ -133,11 +103,16 @@ def pre_process(mouse_ids=None, sessions=None, data_dir = '/data/'):
             print(f'column: {column}')
             print(f'volume: {volume}')
             session_dir = os.path.join(mouse_dir, session)
+
             nwb_file = [file for file in os.listdir(session_dir) if 'nwb' in file][0]
             nwb_path = os.path.join(session_dir, nwb_file)
             with NWBZarrIO(str(nwb_path), 'r') as io:
                 nwbfile = io.read()
-            print(f'loaded and running interp on {nwb_path}')
+
+            stim_table = pull_stim_info(nwbfile)
+            stim_cols = ["stim_name", "start_time", "stop_time", "duration"]
+            stim_lists = {c: stim_table[c].tolist() for c in stim_cols}
+            
             interp = interp_across_planes(nwbfile, column, volume, remove_known_bad_planes=True)
             cell_df.append({
                     "mouse_id": mouse,
@@ -145,11 +120,15 @@ def pre_process(mouse_ids=None, sessions=None, data_dir = '/data/'):
                     "column": column,
                     "volume": volume,
                     **interp,
+                    **stim_lists
             })
             print(f'added {session} to cell_df')
-    return cell_df
+    return pd.DataFrame(cell_df)
 
 #%%
-df = pd.DataFrame(cell_df)
-df.to_pickle('cell_table.pkl') 
-    
+df = pd.DataFrame(stim_df) 
+out_dir = "/root/capsule/scratch"
+os.makedirs(out_dir, exist_ok=True)
+out_path = os.path.join(out_dir, "stim_table.pkl")
+df.to_pickle(out_path, protocol=pickle.HIGHEST_PROTOCOL)
+print(f"Saved to {out_path}")
