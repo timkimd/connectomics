c51f517581c19e83965850568362f9ea6e61d9f6
sashatinelli439 <sasha.tinelli@chch.ox.ac.uk>
2025-09-02 05:14:08 +0000

    building functionality to preprocess all data

--- code/scripts/load_functional_data.py
+++ code/scripts/load_functional_data.py
@@ -15,6 +15,7 @@ import glob
 import collections
 from hdmf_zarr import NWBZarrIO
 from nwbwidgets import nwb2widget
+import tqdm as tqdm
 
 # Set the utils path
 utils_dir = pjoin("..", "utils")
@@ -28,26 +29,23 @@ from data_io import _get_data_dir
 #%% get metadata table
 data_dir = '/data/'
 metadata = pd.read_csv(os.path.join(data_dir, 'metadata', 'V1DD_metadata.csv'))
-metadata.head()
 
 #%% get subject ids
 subject_ids = np.sort(metadata['subject_id'].unique())
 subject_id = subject_ids[0]
-print('Selected subject_id is', subject_id)
 
 #%% get one mouse metadata
 this_mouse_metadata = metadata[metadata['subject_id']==subject_id].sort_values(by='session_date')
-#this_mouse_metadata.head()
 
 #%% get this mouse directory
 data_dir = '/data/'
-mouse_id = '409828'
+mouse_id = str(subject_id)
 mouse_dir = glob.glob((os.path.join(data_dir, mouse_id+'*')))[0]
 mouse_dir
 
 #%% get session
 session_name = this_mouse_metadata.name.values[-1]
-print('Selected session is', session_name)
+session_name
 
 #%% get session dir
 session_dir = os.path.join(mouse_dir, session_name)
@@ -79,7 +77,6 @@ def get_sessions(mouse_id, data_dir):
     return sorted(session_names)
 
 #%% preprocess data function
-##add for session loop, save per session, or save to monster table with session information
 def interp_across_planes(column, volume, remove_known_bad_planes=True):
     planes = range(6)
     if remove_known_bad_planes:
@@ -92,15 +89,16 @@ def interp_across_planes(column, volume, remove_known_bad_planes=True):
 
     base_times = nwbfile.processing["plane-0"].data_interfaces["dff"].timestamps
     for plane in planes:
-        timestamps = nwbfile.processing[f"plane-{plane}"].data_interfaces["dff"].timestamps
-        traces_xarray = nwbfile.processing[f"plane-{plane}"].data_interfaces["dff"].data
-        roi_table = nwbfile.processing[f"plane-{plane}"].data_interfaces["dff"].rois.to_dataframe()
-        roi = roi_table.roi
+        rois = nwbfile.processing[f"plane-{plane}"].data_interfaces["image_segmentation"]["roi_table"][:]
+        good_rois = rois[rois.is_soma==True].roi.values
+        print(f'good ROIs = {len(good_rois)})
+        traces_xarray = nwbfile.processing[f"plane-{plane}"].data_interfaces["dff"].data[:,good_rois]
+        timestamps = nwbfile.processing[f"plane-{plane}"].data_interfaces["dff"].timestamps[:]
         f_interp = interpolate.interp1d(timestamps, traces_xarray, 
                             kind='linear', axis=0, bounds_error=False, fill_value="extrapolate")
-        (dff_traces.extend(f_interp(base_times).T))
-        roi_traces.extend(roi)
-        planes_traces.extend([plane]* len(roi))
+        dff_traces.extend(f_interp(base_times).T)
+        roi_traces.extend(good_rois)
+        planes_traces.extend([plane]* len(good_rois))
 
     dff_traces = np.array(dff_traces)
     planes_traces = np.array(planes_traces)
@@ -113,12 +111,67 @@ def interp_across_planes(column, volume, remove_known_bad_planes=True):
         "base_time": base_times,
     }
 
-#%% loop over sessions
-mouse_id = '409828'
-data_dir = '/data/'
-sessions = get_sessions(mouse_id, data_dir)
-cell_df = []
+#%% get column and volumes
+col_vol = []
+for col in this_mouse_metadata["column"].unique():
+    for vol in this_mouse_metadata.loc[this_mouse_metadata["column"] == col, "volume"].unique():
+        if pd.isna(vol): 
+            continue
+        else:
+            col_vol.append([col, vol])
+col_vol[0]
 
-for i, session in enumerate(sessions)
-    interp = interp_across_planes(column, volume, remove_known_bad_planes=True)
+#%% loop over sessions
+mouse_ids = ['409828']
+sessions = ['409828_2018-11-06_14-02-59_nwb_2025-08-08_16-27-52']
+def pre_process(mouse_ids=None, sessions=None, data_dir = '/data/'):
+    """
+    mouse_ids: str | list[str] | None
+      - Single mouse id, a list of mouse ids, or None (defaults to ['409828']).
+
+    sessions: str | list[str] | dict[str, str | list[str]] | None
+      - If None: process all sessions returned by get_sessions(mouse, data_dir).
+      - If str: process just that one session for all mice.
+      - If list[str]: process that subset for all mice.
+      - If dict[mouse_id -> str | list[str]]: per-mouse selection.
+    """
+    if mouse_ids is None:
+        mouse_ids = ['409828']#, '416296', '427836', '438833']
+
+    cell_df = []
+    for k, mouse in enumerate(tqdm.tqdm(mouse_ids, leave=False, desc='mouse_ids')):
+        mouse_dir = glob.glob(os.path.join(data_dir, mouse + '*'))[0]
+        all_sessions = get_sessions(mouse, data_dir)
+        if isinstance(sessions, dict):
+            requested = _to_list(sessions.get(mouse), default=all_sessions)
+        else:
+            requested = _to_list(sessions, default=all_sessions)
+
+        # keep only those sessions you want 
+        use_sessions = [s for s in requested if s in all_sessions]
+        missing = sorted(set(requested) - set(use_sessions))
+        if missing:
+            print(f'Warning: {mouse} missing sessions: {missing}')
+        for column, volume in tqdm.tqdm(col_vol, desc ="col/vol"):
+            for i, session in enumerate(tqdm.tqdm(use_sessions, leave=False, desc='sessions')):
+                session_dir = os.path.join(mouse_dir, session)
+                nwb_file = [file for file in os.listdir(session_dir) if 'nwb' in file][0]
+                nwb_path = os.path.join(session_dir, nwb_file)
+                with NWBZarrIO(str(nwb_path), 'r') as io:
+                    nwbfile = io.read()
+                print(f'loaded and running interp on {nwb_path}')
+                interp = interp_across_planes(column, volume, remove_known_bad_planes=True)
+                cell_df.append({
+                        "mouse_id": mouse,
+                        "session_id": session,
+                        "column": column,
+                        "volume": volume,
+                        **interp,
+                })
+                print(f'added {session} to cell_df')
+    return cell_df
+
+#%%
+df = pd.DataFrame(cell_df)
+df.to_pickle('cell_table.pkl') 
     
